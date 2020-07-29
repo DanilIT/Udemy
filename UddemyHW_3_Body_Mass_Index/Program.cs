305e2bf8f953d046aa007fb64d734f4608000a1e
Danil <dan_peace@mail.ru>
2020-07-29 20:17:22 +0300

    added 3rd homework, body mass inddex

---
+++ UddemyHW_3_Body_Mass_Index/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UddemyHW_3_Body_Mass_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Body Mass Index calculating programm");
+            Console.WriteLine("Please enter your last name:\t");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your first name:\t");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age:\t");
+            double age = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your weight:\t");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your height:\t");
+            double height = double.Parse(Console.ReadLine());
+
+            double bodyMassIndex = weight / (height/100 * height/100);
+
+            Console.WriteLine($"Your profile: \nFull Name: \t{lastName} {firstName} \n" +
+                $"Age: \t{age} \nWeight: \t{weight} \nHeight: \t{height} " +
+                $"\nBody Mass Index: \t{bodyMassIndex}");
+        }
+    }
+}
